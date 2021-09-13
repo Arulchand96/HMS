9d8114b541f0e4366dbcded10cb0a0fb71464634
Arul <arulchand.v@busofttech.com>
2021-09-13 20:34:07 +0530

    initial commit

--- src/main/java/utility/DateUtility.java
+++ src/main/java/utility/DateUtility.java
@@ -50,7 +50,7 @@ public class DateUtility {
     }
 
     public static String convertDateToString(Date date) {
-        if(date==null) {
+        if (date == null) {
             date = Calendar.getInstance().getTime();
         }
         DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
@@ -60,27 +60,26 @@ public class DateUtility {
 
     }
 
-    public static Date getStringToDate(String stringDate)   {
+    public static Date convertStringToDate(String stringDate) {
 
-        Date date1=null;
+        Date date1 = null;
 
         try {
 
-             date1 = new SimpleDateFormat("MM/dd/yyyy").parse(stringDate);
-        }
-        catch(Exception e) {
+            date1 = new SimpleDateFormat("MM/dd/yyyy").parse(stringDate);
+        } catch (Exception e) {
 
-            date1=Calendar.getInstance().getTime();
+            date1 = Calendar.getInstance().getTime();
 
         }
 
         return date1;
     }
 
-    public static  Date lastDate(String stringDate) throws Exception {
+    public static Date lastDate(String stringDate) throws Exception {
 
         //Date date1=new SimpleDateFormat("MM/dd/yyyy").parse(stringDate);
-        Date date1=getStringToDate(stringDate);
+        Date date1 = getStringToDate(stringDate);
 
         Calendar c = Calendar.getInstance();
         c.setTime(date1);
@@ -88,16 +87,160 @@ public class DateUtility {
         return c.getTime();
     }
 
-    public static  Date firstDate(String stringDate) throws Exception {
+    public static Date firstDate(String stringDate) throws Exception {
 
-        Date date1=getStringToDate(stringDate);
+        Date date1 = getStringToDate(stringDate);
         Calendar c = Calendar.getInstance();
         c.setTime(date1);
         c.set(Calendar.DAY_OF_MONTH, c.getActualMinimum(Calendar.DAY_OF_MONTH));
         return c.getTime();
     }
 
+    public static Date firstYear(String stringDate) {
 
+        Date date1 = getStringToDate(stringDate);
+        Calendar c = Calendar.getInstance();
+        c.setTime(date1);
+        c.set(Calendar.DAY_OF_YEAR, c.getActualMinimum(Calendar.DAY_OF_YEAR));
+        Date date2 = c.getTime();
+        return date2;
+    }
+
+    public static Date lastDateOfYear(String stringDate) {
+
+        Date date1 = getStringToDate(stringDate);
+        Calendar c = Calendar.getInstance();
+        c.setTime(date1);
+        c.set(Calendar.DAY_OF_YEAR, c.getActualMaximum(Calendar.DAY_OF_YEAR));
+        Date date3 = c.getTime();
+        return date3;
+    }
+
+    public static String getStringToString1(String oldString) {
+
+        String newString = null;
+
+        try {
+            SimpleDateFormat dt = new SimpleDateFormat("MM/dd/YYYY");
+            Date date = dt.parse(oldString);
+            SimpleDateFormat dt1 = new SimpleDateFormat("MMMMMM dd, YYYY");
+            newString = dt1.format(date);
+        } catch (Exception e) {
+
+            //date1=Calendar.getInstance().getTime();
+
+        }
+        return newString;
+
+    }
+
+    public static String getStringToString2(String oldString) {
+
+        String newString = null;
+
+        try {
+
+            Date date = new SimpleDateFormat("MM/dd/YYYY").parse(oldString);
+
+            newString = new SimpleDateFormat("MMM dd, YYYY").format(date);
+
+        } catch (Exception e) {
+
+            //date1=Calendar.getInstance().getTime();
+
+        }
+        return newString;
+    }
+
+    public static Date addOneHour(Date date) {
+
+        Calendar cal = Calendar.getInstance();
+        //date= getStringToDate(stringDate);
+        cal.setTime(date);
+        cal.add(Calendar.HOUR_OF_DAY, 1);
+        Date date1 = cal.getTime();
+
+        return date1;
+
+    }
+
+    public static Date subOneHour(Date date) {
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.HOUR_OF_DAY, -1);
+        Date date1 = cal.getTime();
+
+        return date1;
+
+    }
+
+    public static Date addOneDay(Date date) {
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DATE, 1);
+        Date date1 = cal.getTime();
+
+        return date1;
+
+    }
+
+    public static Date subOneDay(Date date) {
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DATE, -1);
+        Date date1 = cal.getTime();
+
+        return date1;
+
+    }
+
+    public static Date getStringToDate(String stringDate) {
+
+        Date date1 = null;
+
+        try {
+
+            date1 = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss").parse(stringDate);
+        } catch (Exception e) {
+
+            date1 = Calendar.getInstance().getTime();
+
+        }
+
+        return date1;
+    }
+
+
+    static long[] findDifference(Date startDate,Date endDate) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+            long difference_In_Time = startDate.getTime() - endDate.getTime();
+
+            long difference_In_Days = (difference_In_Time / (1000 * 60 * 60 * 24)) % 365;
+
+            long difference_In_Hours = (difference_In_Time / (1000 * 60 * 60)) % 24;
+
+            long difference_In_Minutes = (difference_In_Time / (1000 * 60)) % 60;
+
+            long difference_In_Seconds = (difference_In_Time / 1000) % 60;
+
+            long result[]=new long[4];
+
+        result[0] = difference_In_Days;
+        result[1] = difference_In_Hours;
+        result[2] = difference_In_Minutes;
+        result[3] = difference_In_Seconds;
+
+           // System.out.println(difference_In_Days + " days, " + difference_In_Hours + " hours, " + difference_In_Minutes + " minutes, " + difference_In_Seconds + " seconds");
+            return result;
+
+
+
+
+
+
+    }
 }
-
-
--- src/main/java/utility/UtilityTest.java
+++ src/main/java/utility/UtilityTest.java
@@ -1,13 +1,19 @@
 package utility;
 
+import java.net.SocketOption;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.text.ParseException;
 
 public class UtilityTest {
 
     public static void main(String args[]) throws Exception {
 
-        System.out.println(DateUtility.curIntDate());
+
+
+        /*System.out.println(DateUtility.curIntDate());
         System.out.println(DateUtility.curDay());
         System.out.println(DateUtility.curMonth());
         System.out.println(DateUtility.curYear());
@@ -24,8 +30,36 @@ public class UtilityTest {
         String stringDate=input.next();
         //DateTimeFormatter format = DateTimeFormatter
                 //.ofPattern("MM/dd/yyyy");
-        System.out.println("Date Format:"+DateUtility.getStringToDate(stringDate));
+        System.out.println("Date Format:"+DateUtility.getStringToDate(stringDate)); */
 
+        // String stringDate ="18/9/2021";
+        // Date date2=DateUtility.firstYear(stringDate);
+        // System.out.println(date2);
+        /*String stringDate="08/10/2015";
+
+        String date1=DateUtility.getStringToString1(stringDate);
+        System.out.println(date1);
+        Date date=new Date();
+
+        System.out.println(DateUtility.addOneHour(date));
+        System.out.println(DateUtility.subOneHour(date));
+        System.out.println(DateUtility.addOneDay(date));
+        System.out.println(DateUtility.subOneDay(date));*/
+        Scanner input=new Scanner(System.in);
+        System.out.println("Given Date and Time :");
+        String stringDate =input.nextLine();
+        Date date1=DateUtility.getStringToDate(stringDate);
+        System.out.println("add one hour with given date and time:"+DateUtility.addOneHour(date1));
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("Enter the start date and time:");
+        String stringDate1=input.nextLine();
+        System.out.println("Enter the end date and time:");
+        String stringDate2=input.nextLine();
+        Date date2=DateUtility.getStringToDate(stringDate1);
+        Date date3=DateUtility.getStringToDate(stringDate2);
+        DateUtility.findDifference(date2,date3);
+        long result[]=DateUtility.findDifference();
+        System.out.println(DateUtility.findDifference(Arrays.toString(result[0])) + " days, " + difference_In_Hours + " hours, " + difference_In_Minutes + " minutes, " + difference_In_Seconds + " seconds");
 
 
     }
